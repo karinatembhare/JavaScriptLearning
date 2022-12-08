2f0287204f55035c404a6abc1061759500b5c13c
karinatembhare <karinatembhare2000@gmail.com>
2022-12-08 10:02:19 +0530

    add new file
    add new file

---
+++ 01_variable.js
@@ -0,0 +1,9 @@
+var myName;   //variable declaration
+myName="karina tembhare";     //variable initialization//
+console.log(myName);
+
+var myFrndName="neha";  //variable declaration and initialization at same time
+ console.log(myFrndName);  
+ 
+ myFrndName="danny"; //update variable//
+ console.log(myFrndName);
---
+++ 01_variable_assign.js
@@ -0,0 +1,20 @@
+var myFullName           //variable declaration//
+var myFullName="Karina Tembhare";     //initialization//
+console.log(myFullName);       
+
+myFullName="karina pradip tembhare";      //variable declaration and initializing//
+console.log(myFullName);
+
+myLove="I Love only JS";      //update//
+console.log(myLove);
+
+firstName="karina"          //call variable by saperate using comma//
+lastName="tembhare"
+age=21
+address="vikas nagar khadgao road wadi,Nagpur"
+pin_code=440023
+gender="female"
+is_married="no"
+console.log(firstName,lastName,age,address,pin_code,gender,is_married);
+
+
